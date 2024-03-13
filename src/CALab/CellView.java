1167b819216a5e37decbc87c50322f62f29e915f
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-03-12 20:09:25 -0700

    some changes to AppPanel and some other classes

--- src/CALab/CellView.java
+++ src/CALab/CellView.java
@@ -1,11 +1,31 @@
 package CALab;
 
-import mvc.Model;
-import mvc.View;
+import mvc.Subscriber;
 
-public class CellView extends View {
-    public CellView(Model cell) {
-        super(cell);
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class CellView extends JButton implements ActionListener, Subscriber {
+    Cell myCell;
+    public CellView(Cell cell) {
+        myCell = cell;
     }
+
+    public void actionPerformed(ActionEvent e) {
+        myCell.nextState();
+        setBackground(myCell.getColor());
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        setText("" + myCell.getStatus());
+    } //copypasted from assignment instructions
+    //nextState, getColor, and getStatus should be abstract methods in the Cell class
+
+    @Override
+    public void update() {
+
+    }
+
+
+
+
 
 }
