cdba59cd1cdbcc84c9f6e16117d45af7674debb1
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-03-12 20:16:05 -0700

    minor change for CellView

--- src/CALab/CellView.java
+++ src/CALab/CellView.java
@@ -3,6 +3,7 @@ package CALab;
 import mvc.Subscriber;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class CellView extends JButton implements ActionListener, Subscriber {
@@ -14,7 +15,7 @@ public class CellView extends JButton implements ActionListener, Subscriber {
     public void actionPerformed(ActionEvent e) {
         myCell.nextState();
         setBackground(myCell.getColor());
-        setBorder(BorderFactory.createLineBorder(Color.black));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
         setText("" + myCell.getStatus());
     } //copypasted from assignment instructions
     //nextState, getColor, and getStatus should be abstract methods in the Cell class
