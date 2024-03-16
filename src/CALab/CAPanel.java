7299e72ecae6d0c0b70bae6806894a71c47e8aa4
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-03-16 13:12:12 -0700

    refactors

--- src/CALab/CAPanel.java
+++ src/CALab/CAPanel.java
@@ -1,12 +1,34 @@
 package CALab;
 
+import life.LifeFactory;
 import mvc.AppFactory;
 import mvc.AppPanel;
+
+import javax.swing.*;
 
 public class CAPanel extends AppPanel {
 
     public CAPanel(AppFactory factory) {
         super(factory);
+        JButton run1 = new JButton("RUN1");
+        run1.addActionListener(this);
+        JButton run50 = new JButton("RUN50");
+        run50.addActionListener(this);
+        JButton clear = new JButton("CLEAR");
+        clear.addActionListener(this);
+        JButton populate = new JButton("POPULATE");
+        populate.addActionListener(this);
+        controlPanel.add(run1);
+        controlPanel.add(run50);
+        controlPanel.add(clear);
+        controlPanel.add(populate);
+
+    }
+
+    public static void main(String[] args) {
+        AppFactory factory = new LifeFactory(); // Change might be needed
+        AppPanel panel = new CAPanel(factory);
+        panel.display();
     }
 
 }
